7e5b2cbac1fb749f0f7f93f220c096bd50fe9a2d
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2021-01-12 00:04:23 +0100

    AlphaBeta Puning ausgebessert + Vergleichen

--- app/src/main/java/net/ubung/tictactoe_alpha_betapruning/MiniMax.java
+++ app/src/main/java/net/ubung/tictactoe_alpha_betapruning/MiniMax.java
@@ -3,7 +3,8 @@ package net.ubung.tictactoe_alpha_betapruning;
 public class MiniMax {
 
     private  Spieler player = new Spieler(1, "X");
-    private  String comp = "O";
+    private  final String COMP = "O";
+    public static int counter = 0;
 
 
     // überprüft ob noch freie Felder vorhanden sind -- noch weitere steps notwendig sind
@@ -23,7 +24,7 @@ public class MiniMax {
             if (field[row][0].equals(field[row][1]) && field[row][1].equals(field[row][2])) {
                 if (field[row][0].equals(player.getPref())) {
                     return -1;
-                } else if (field[row][0].equals(comp))
+                } else if (field[row][0].equals(COMP))
                     return 1;
             }
         }
@@ -33,7 +34,7 @@ public class MiniMax {
             if (field[0][col].equals(field[1][col]) && field[1][col].equals(field[2][col])) {
                 if (field[0][col].equals(player.getPref())) {
                     return -1;
-                } else if (field[0][col].equals(comp)) {
+                } else if (field[0][col].equals(COMP)) {
                     return 1;
                 }
             }
@@ -43,14 +44,14 @@ public class MiniMax {
         if (field[0][0].equals(field[1][1]) && field[1][1].equals(field[2][2])) {
             if (field[0][0].equals(player.getPref())){
                 return -1;}
-            else if (field[0][0].equals(comp)){
+            else if (field[0][0].equals(COMP)){
                 return 1;}
         }
 
         if (field[0][2].equals(field[1][1]) && field[1][1].equals(field[2][0])) {
             if (field[0][2].equals(player.getPref())){
                 return -1;}
-            else if (field[0][2].equals(comp)){
+            else if (field[0][2].equals(COMP)){
                 return 1;}
         }
 
@@ -59,46 +60,46 @@ public class MiniMax {
 
     }
     
-    private int alphaBeta(String[][] field, int depth,  boolean isMax, int alpha, int beta){
+    private int alphaBeta(String[][] field,  boolean isMax, int alpha, int beta){
+
         int score = checkWinner(field);//wie bei minimax
         if (score == 1){
             return score;}
         else if (score == -1){
             return score;}
-        else if (isFieldLeft(field) == false){
+        else if (!isFieldLeft(field)){
             return 0;}
-        
-        if(isMax){
-            int best = Integer.MIN_VALUE;
-            for(int i = 0; i<field.length; i++){
-                for (int j = 0; j <field.length ; j++) {
-                    if(field[i][j].equals("")) {
-                        field[i][j] = comp; //wie bei minimax -> computer option setzen
-                        int val = alphaBeta(field, depth + 1, !isMax, alpha, beta);
-                        best = Math.max(best, val);
-                        alpha = Math.max(alpha, best);
+
+        if (isMax) {
+            int best =alpha;
+            aussen:
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (field[i][j].equals("")) {
+                        field[i][j] = COMP;//eigenes feld setzen  und best mögliches ergebnis speichern
+                        best = Math.max(best, alphaBeta(field,  false, best, beta));
+                        MiniMax.counter++;
                         field[i][j] = "";// Feld zurücksetzen
-                    }
-                    if(best<=alpha){
-                        break;
+                        if(best >= beta){
+                            break aussen;
+                        }
                     }
                 }
             }
             return best;
-        }
-        else{
-            int best = Integer.MAX_VALUE;
-            for(int i = 0; i<field.length; i++){
-                for (int j = 0; j <field.length ; j++) {
-                    if(field[i][j].equals("")) {
-                        field[i][j] = player.getPref(); //wie bei minimax -> Gegener option setzen
-                        int val = alphaBeta(field, depth + 1, !isMax, alpha, beta);
-                        best = Math.min(best, val);
-                        alpha = Math.min(alpha, best);
-                        field[i][j] = "";// Feld zurücksetzen
+        } else {//wen min dran ist
+            int best = beta;
+            aussen:
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (field[i][j].equals("")) {
+                        field[i][j] = player.getPref(); //feld des gegners setzen und schlecht mögliches ergebnis speichern
+                        best = Math.min(best, alphaBeta(field,  true, alpha, best));
+                        MiniMax.counter++;
+                        field[i][j] = "";//Feld wieder zurücksetzen
+                        if (best <= alpha){
+                            break aussen;
                     }
-                    if(best<=alpha){
-                        break;
                     }
                 }
             }
@@ -108,14 +109,14 @@ public class MiniMax {
 
 
     //überprüft alle möglichen Wege, wie das Spiel enden kann
-    private int minimax(String[][] field, int depth, boolean isMax) {
-       //bewertet die Züge mit punkten(je nachdem ob minimax gewonnen hat oder nicht)
+    private int minimax(String[][] field, boolean isMax) {
+
         int score = checkWinner(field);
         if (score == 1){
             return score;}
         else if (score == -1){
             return score;}
-        else if (isFieldLeft(field) == false){
+        else if (!isFieldLeft(field)){
             return 0;}
 
 
@@ -125,8 +126,9 @@ public class MiniMax {
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     if (field[i][j].equals("")) {
-                        field[i][j] = comp;//eigenes feld setzen  und best mögliches ergebnis speichern
-                        best = Math.max(best, minimax(field, depth + 1, !isMax));
+                        field[i][j] = COMP;//eigenes feld setzen  und best mögliches ergebnis speichern
+                        best = Math.max(best, minimax(field,  !isMax));
+                        MiniMax.counter++;
                         field[i][j] = "";// Feld zurücksetzen
                     }
                 }
@@ -138,7 +140,8 @@ public class MiniMax {
                 for (int j = 0; j < 3; j++) {
                     if (field[i][j].equals("")) {
                         field[i][j] = player.getPref(); //feld des gegners setzen und schlecht mögliches ergebnis speichern
-                        best = Math.min(best, minimax(field, depth + 1, !isMax));
+                        best = Math.min(best, minimax(field,  !isMax));
+                        MiniMax.counter++;
                         field[i][j] = "";//Feld wieder zurücksetzen
                     }
                 }
@@ -149,17 +152,21 @@ public class MiniMax {
 
 
     public  Move findBestMove(String[][] field){
+       MiniMax.counter = 0;
         int bestVal = Integer.MIN_VALUE;
         Move bestMove = new Move();
         bestMove.setCol(-1);
         bestMove.setRow(-1);
 
+
         for (int i = 0; i < 3; i++) {//sucht mit der minimax-Methode den besten Zug aus und gibt ihn aus
             for (int j = 0; j < 3; j++) {
                 if (field[i][j].equals("")){
-                    field[i][j]=comp;
-                    //int moveVal = minimax(field, 0, false);
-                    int moveVal = alphaBeta(field,0,true,-1000,1000);
+                    field[i][j]=COMP;
+                   // int moveVal = minimax(field, false);
+                    int moveVal = alphaBeta(field,false,Integer.MIN_VALUE,Integer.MAX_VALUE);
+
+
                     field[i][j] = "";
                     if(moveVal > bestVal){
                         bestMove.setRow(i);
@@ -171,6 +178,7 @@ public class MiniMax {
             }
 
         }
+        System.out.println(MiniMax.counter);
         return bestMove;
 
     }
